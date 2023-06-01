137859358f2bd33b7b716d9d5431516b4de17795
kelyan-9560 <kelyan.bervin@gmail.com>
2023-06-01 20:57:01 +0200

    Create InstructionTest.java

---
+++ src/test/java/models/InstructionTest.java
@@ -0,0 +1,26 @@
+package models;
+
+import exceptions.InstructionsException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstructionTest {
+
+    @Test
+    @DisplayName("should throws an InstructionsException() when instruction is not found")
+    void should_throws_an_exception_when_instruction_is_found() {
+        assertThrows(
+                InstructionsException.class,
+                () -> new Instruction("Z"));
+    }
+
+    @Test
+    @DisplayName("should throws an InstructionsException() when instruction letter is too long")
+    void should_throws_an_exception_when_instruction_letter_is_to_long() {
+        assertThrows(
+                InstructionsException.class,
+                () -> new Instruction("AA"));
+    }
+}
